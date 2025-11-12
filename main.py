be0d4d4f8728d2b645ffdfb01bdfbf188be0362f
Do Duc Thang <161011006+V1ctorious3010@users.noreply.github.com>
2025-11-12 23:33:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def linear_evaluation(backbone, X_train, y_train, X_test, y_test, n_outputs, arg
 if __name__ == '__main__':
     args = parser.parse_args()
     all_results = []
-    for group in range(1, 1):
+    for group in range(1, 2):
         x_train, y_train, x_test, y_test  = get_data(args.dataset, group)
         n_timesteps, n_features, n_outputs = x_train.shape[1], x_train.shape[2], y_train.shape[1]
     
@@ -63,10 +63,12 @@ if __name__ == '__main__':
         print("\n--- Starting Unsupervised Pre-training ---")
         train(model,x_train,args)
         print("\n--- Starting Linear Evaluation ---")
-        f1_score = linear_evaluation(backbone, x_train, y_train, x_test, y_test, n_outputs, args)
-        all_results.append({'group': group, 'f1_score': f1_score})
-        df_results = pd.DataFrame(all_results)
-        df_results.to_csv("results.csv", index=False)
+        f1 = linear_evaluation(backbone, x_train, y_train, x_test, y_test, n_outputs, args)
+        all_results.append({'group': group, 'f1_score': f1})
+        
+    df_results = pd.DataFrame(all_results)
+    df_results.to_csv("results.csv", index=False)
     
+
 
 
